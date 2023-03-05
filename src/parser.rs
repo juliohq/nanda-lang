86d4315dae33b0ba16628c0efec3fcef6a2b522e
juliohq <juliohenrique31501234@gmail.com>
2023-03-05 13:43:54 -0300

    Replace parenthesis

--- src/parser.rs
+++ src/parser.rs
@@ -6,8 +6,47 @@ pub struct Parser;
 impl Parser {
     #[inline]
     pub fn run(source: String) {
+        // Static source code analysis
+        // Replace parenthesis
+        let mut is_string = false; // Whether there's an open string
+        let mut replace_line: usize = 1;
+        let mut last_string: usize = 0; // Line of the last open string
+
+        let binding: String = source
+            .chars()
+            .map(|char| {
+                if char == '\n' {
+                    // Increase replace line counter
+                    replace_line += 1;
+                }
+
+                if is_string {
+                    // Close string
+                    if char == '\'' || char == '\"' {
+                        is_string = false;
+                    }
+
+                    // Just return the character
+                    char
+                } else {
+                    // Open string
+                    if char == '\'' || char == '\"' {
+                        last_string = replace_line;
+                        is_string = true;
+                    }
+
+                    char
+                }
+            })
+            .collect();
+
+        // Make sure all strings were closed
+        if is_string {
+            println!("String not enclosed. (line {})", last_string);
+        }
+
         // Split source code by line endings and filter single comment lines
-        let lines = source
+        let lines = binding
             .split("\n")
             .map(|line| if line.starts_with("//") { "\n" } else { line });
 
@@ -17,6 +56,7 @@ impl Parser {
         // Line counter
         let mut counter = 1u64;
 
+        // Run source code
         for line in lines {
             // Split parts (a.k.a. [let, x, =, "hello"])
             let mut parts = line.split_whitespace();
